ac6054fc7e729f7e8e9af7bf406ab16c789523ef
Gustavo Sampaio Soier <gsampaiosoier0@gmail.com>
2023-10-06 19:02:47 -0300

    Clínica Dentista
    Terminei a seção de "Dentistas" e  a seção de "Serviços".

--- js/script.js
+++ js/script.js
@@ -1,14 +1,19 @@
-window.onscroll = function(){
-    var top = window.scrollY || document.documentElement.scrollTop;
-    console.log(top);
+// window.onscroll = function(){
+//     var top = window.scrollY || document.documentElement.scrollTop;
+//     console.log(top);
   
-    if(top > 800){
-      console.log("Adicionar meu menu fixo");
-      document.getElementById("topoFixo").classList.add("menu-fixo")
-    }
+//     if(top > 800){
+//       console.log("Adicionar meu menu fixo");
+//       document.getElementById("topoFixo").classList.add("menu-fixo")
+//     }
   
-    else{
-      console.log("Remover meu menu fixo");
-      document.getElementById("topoFixo").classList.remove("menu-fixo")
-    }
+//     else{
+//       console.log("Remover meu menu fixo");
+//       document.getElementById("topoFixo").classList.remove("menu-fixo")
+//     }
+// }
+
+function linkWhats(){
+  var numFone = '5511951694143'
+  window.open('https://api.whatsapp.com/send?phone=' + numFone)
 }
