427e1a5c5183b9748e11540efc617aa9da40f626
Gustavo Sampaio Soier <gsampaiosoier0@gmail.com>
2023-10-06 23:15:25 -0300

    Clínica Dentista
    Mudei o scroll do navegador e adicionei uma seta na seção "topo" que leva para seção "sobre".

--- js/script.js
+++ js/script.js
@@ -17,3 +17,15 @@ function linkWhats(){
   var numFone = '5511951694143'
   window.open('https://api.whatsapp.com/send?phone=' + numFone)
 }
+
+    document.querySelector('.arrow-down a').addEventListener('click', function (e) {
+        e.preventDefault(); // Impede o comportamento padrão do link
+
+        const targetSection = document.querySelector('#sobre');
+        const targetSectionPosition = targetSection.offsetTop; // Posição da próxima seção
+
+        window.scrollTo({
+            top: targetSectionPosition,
+            behavior: 'smooth' // Rola suavemente
+        });
+    });
