267d4f2e379379afaeeff3eb825a3e1910c04ae5
GustaSoier <gsampaiosoier0@gmail.com>
2023-10-27 22:56:37 -0300

    Clínica Dentista
    Passando o código para PHP

---
+++ conteudo/banner.php
@@ -0,0 +1,16 @@
+    <section class="banner">
+        <div>
+            <h2>
+                Clínica Dentista
+            </h2>
+
+            <a href="#">Marque uma consulta</a>
+        </div>
+
+            <span class="seta-abaixo">
+                <a href="#sobre">
+                    <i class="ri-arrow-down-double-line"></i>
+                </a>
+            </span>
+        
+    </section>
---
+++ conteudo/sobre.php
@@ -0,0 +1,15 @@
+    <section id="sobre" class="sobre site">
+        <div>
+
+            <div>
+                <h2>Sobre nossa <span> Clínica Dentista</span></h2>
+                <p>Bem-vindo à Clínica Dentária [Nome da Sua Clínica], onde transformamos sorrisos e vidas. Nossa missão é ir além do convencional, proporcionando um ambiente acolhedor e tratamento personalizado. Contamos com uma equipe apaixonada, unindo perícia técnica com empatia. Oferecemos serviços odontológicos abrangentes, do preventivo ao estético. Nosso compromisso é garantir transparência, educação e conforto em cada etapa do tratamento. Junte-se a nós nessa jornada por um sorriso saudável e radiante na Clínica Dentária [Nome da Sua Clínica]. Cada sorriso conta uma história única, e nós valorizamos todas. Agende sua consulta e permita-nos transformar seu sorriso. Seja bem-vindo à Clínica Dentária [Nome da Sua Clínica], onde cada sorriso é prioridade.</p>
+                <a href="#">Saiba mais</a>
+            </div>
+
+            <div>
+                <img src="img/img-sobre.webp" alt="">
+            </div>
+
+        </div>
+    </section>
