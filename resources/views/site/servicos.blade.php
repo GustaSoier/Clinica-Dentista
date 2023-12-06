d1b0f3e6294854b0ff884cdfc0e3abf580deea9a
GustaSoier <gsampaiosoier0@gmail.com>
2023-12-06 20:39:40 -0300

    Clínica dentista
    subindo arquivos do laravel

---
+++ resources/views/site/servicos.blade.php
@@ -0,0 +1,10 @@
+@extends('layout.layout'){{-- para extender da página layout--}}
+
+@section('title', 'Serviços e Planos')
+
+
+@section('conteudo')
+
+    <h1>Página Serviços e Planos</h1>
+
+@endsection
