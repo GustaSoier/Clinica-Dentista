5f28ce5096b54f8a6cfa5d270d40175b231ed924
Gustavo <gsampaiosoier0@gmail.com>
2024-01-26 02:08:41 -0300

    PHP
    Mudança do layout.blade.php

--- clinicadentista/resources/views/layout/layout.blade.php
+++ clinicadentista/resources/views/layout/layout.blade.php
@@ -1,205 +1,176 @@
 <!DOCTYPE html>
 <html lang="pt-br">
     <head>
-        <title>@yield('title') - DentalCare</title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<!-- Mobile Specific Meta -->
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<!-- Favicon-->
+		<link rel="shortcut icon" href="img/fav.png">
+		<!-- Author Meta -->
+		<meta name="author" content="colorlib">
+		<!-- Meta Description -->
+		<meta name="description" content="">
+		<!-- Meta Keyword -->
+		<meta name="keywords" content="">
+		<!-- meta character set -->
+		<meta charset="UTF-8">
+		<!-- Site Title -->
+		<title>@yield('title') - DentalCare</title>
 
-        <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
-
-        <link rel="stylesheet" href="{{ asset ('css/open-iconic-bootstrap.min.css') }}">
-        <link rel="stylesheet" href="{{ asset ('css/animate.css') }}">
-
-        <link rel="stylesheet" href="{{ asset ('css/owl.carousel.min.css') }}">
-        <link rel="stylesheet" href="{{ asset ('css/owl.theme.default.min.css') }}">
-        <link rel="stylesheet" href="{{ asset ('css/magnific-popup.css') }}">
-
-        <link rel="stylesheet" href="{{ asset ('css/aos.css') }}">
-
-        <link rel="stylesheet" href="{{ asset ('css/ionicons.min.css') }}">
-
-        <link rel="stylesheet" href="{{ asset ('css/bootstrap-datepicker.css') }}">
-        <link rel="stylesheet" href="{{ asset ('css/jquery.timepicker.css') }}">
-
-
-        <link rel="stylesheet" href="{{ asset('css/flaticon.css') }}">
-        <link rel="stylesheet" href="{{ asset ('css/icomoon.css') }}">
-        <link rel="stylesheet" href="{{ asset ('css/style.css') }}">
+		<link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet">
+			<!--
+			CSS
+			============================================= -->
+			<link rel="stylesheet" href="css/linearicons.css">
+			<link rel="stylesheet" href="css/font-awesome.min.css">
+			<link rel="stylesheet" href="css/bootstrap.css">
+			<link rel="stylesheet" href="css/magnific-popup.css">
+			<link rel="stylesheet" href="css/nice-select.css">
+			<link rel="stylesheet" href="css/animate.min.css">
+			<link rel="stylesheet" href="css/jquery-ui.css">
+			<link rel="stylesheet" href="css/owl.carousel.css">
+			<link rel="stylesheet" href="css/main.css">
       </head>
 <body>
 
-    <div class="py-md-5 py-4 border-bottom">
-    	<div class="container">
-    		<div class="row no-gutters d-flex align-items-start align-items-center px-3 px-md-0">
-    			<div class="col-md-4 order-md-2 mb-2 mb-md-0 align-items-center text-center">
-		    		<a class="navbar-brand" href="{{ url('/home') }}">Dentista<span>Dental Clinic</span></a>
-	    		</div>
-	    		<div class="col-md-4 order-md-1 d-flex topper mb-md-0 mb-2 align-items-center text-md-right">
-	    			<div class="icon d-flex justify-content-center align-items-center order-md-last">
-	    				<span class="icon-map"></span>
-	    			</div>
-	    			<div class="pr-md-4 pl-md-0 pl-3 text">
-					    <p class="con"><span>Free Call</span> <span>+1 234 456 78910</span></p>
-					    <p class="con">198 West 21th Street, Suite 721 New York NY 10016</p>
-				    </div>
-			    </div>
-			    <div class="col-md-4 order-md-3 d-flex topper mb-md-0 align-items-center">
-			    	<div class="icon d-flex justify-content-center align-items-center"><span class="icon-paper-plane"></span></div>
-			    	<div class="text pl-3 pl-md-3">
-					    <p class="hr"><span>Open Hours</span></p>
-					    <p class="time"><span>Mon - Sat:</span> <span>8:00am - 9:00pm</span> Sun: Closed</p>
-				    </div>
-			    </div>
-		    </div>
-		  </div>
-    </div>
-	  <nav class="navbar navbar-expand-lg navbar-dark bg-dark ftco-navbar-light" id="ftco-navbar">
-	    <div class="container d-flex align-items-center">
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
-	        <span class="oi oi-menu"></span> Menu
-	      </button>
-	      <div class="collapse navbar-collapse" id="ftco-nav">
-	        <ul class="navbar-nav m-auto">
-	        	<li class="nav-item active"><a href="{{ url ('/') }}" class="nav-link pl-0">Home</a></li>
-	        	<li class="nav-item"><a href="{{ url ('/sobre') }}" class="nav-link">Sobre</a></li>
-	        	<li class="nav-item"><a href="doctor.html" class="nav-link">Doctor</a></li>
-	        	<li class="nav-item"><a href="department.html" class="nav-link">Treatments</a></li>
-	        	<li class="nav-item"><a href="pricing.html" class="nav-link">Pricing</a></li>
-	        	<li class="nav-item"><a href="blog.html" class="nav-link">Blog</a></li>
-	          <li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
-	        </ul>
-	      </div>
-	    </div>
-	  </nav>
-    <!-- END nav -->
+    <header id="header" id="home">
+        <div class="header-top">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-6 col-sm-6 col-4 header-top-left no-padding">
+                      <a href="index.html"><img src="img/logo.png" alt="" title="" /></a>
+                    </div>
+                    <div class="col-lg-6 col-sm-6 col-8 header-top-right no-padding">
+                      <a class="btns" href="tel:+953 012 3654 896">+953 012 3654 896</a>
+                        <a class="btns" href="mailto:support@colorlib.com">support@colorlib.com</a>
+                        <a class="icons" href="tel:+953 012 3654 896">
+                            <span class="lnr lnr-phone-handset"></span>
+                        </a>
+                        <a class="icons" href="mailto:support@colorlib.com">
+                            <span class="lnr lnr-envelope"></span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+      </div>
+      <div class="container main-menu">
+          <div class="row align-items-center justify-content-between d-flex">
+            <nav id="nav-menu-container">
+              <ul class="nav-menu">
+                <li class="menu-active"><a href="index.html">Home</a></li>
+                <li><a href="about.html">About</a></li>
+                <li><a href="services.html">Services</a></li>
+                <li><a href="opening-hour.html">Opening Hour </a></li>
+                <li class="menu-has-children"><a href="">Blog</a>
+                  <ul>
+                    <li><a href="blog-home.html">Blog Home</a></li>
+                    <li><a href="blog-single.html">Blog Single</a></li>
+                    <li class="menu-has-children"><a href="">Level 2</a>
+                      <ul>
+                        <li><a href="#">Item One</a></li>
+                        <li><a href="#">Item Two</a></li>
+                      </ul>
+                    </li>
+                  </ul>
+                </li>
+                <li><a href="pricing.html">Pricing</a></li>
+                <li><a href="elements.html">Elements</a></li>
+                <li><a href="contact.html">Contact</a></li>
+              </ul>
+            </nav><!-- #nav-menu-container -->
+            <div class="menu-social-icons">
+              <a href="#"><i class="fa fa-facebook"></i></a>
+              <a href="#"><i class="fa fa-twitter"></i></a>
+              <a href="#"><i class="fa fa-dribbble"></i></a>
+              <a href="#"><i class="fa fa-behance"></i></a>
+          </div>
+          </div>
+      </div>
+    </header><!-- #header -->
 
     <main class="container">
         @yield('conteudo')
     </main>
 
-    <footer class="ftco-footer ftco-bg-dark ftco-section">
-        <div class="container">
-          <div class="row mb-5">
-            <div class="col-md">
-              <div class="ftco-footer-widget mb-5">
-                <h2 class="ftco-heading-2 logo">Dentista</h2>
-                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-              </div>
-              <div class="ftco-footer-widget mb-5">
-                  <h2 class="ftco-heading-2">Have a Questions?</h2>
-                  <div class="block-23 mb-3">
-                    <ul>
-                      <li><span class="icon icon-map-marker"></span><span class="text">203 Fake St. Mountain View, San Francisco, California, USA</span></li>
-                      <li><a href="#"><span class="icon icon-phone"></span><span class="text">+2 392 3929 210</span></a></li>
-                      <li><a href="#"><span class="icon icon-envelope"></span><span class="text">info@yourdomain.com</span></a></li>
-                    </ul>
-                  </div>
+    <!-- start footer Area -->
+			<footer class="footer-area section-gap">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-2  col-md-6">
+							<div class="single-footer-widget">
+								<h6>Top Products</h6>
+								<ul class="footer-nav">
+									<li><a href="#">Managed Website</a></li>
+									<li><a href="#">Manage Reputation</a></li>
+									<li><a href="#">Power Tools</a></li>
+									<li><a href="#">Marketing Service</a></li>
+								</ul>
+							</div>
+						</div>
+						<div class="col-lg-4  col-md-6">
+							<div class="single-footer-widget mail-chimp">
+								<h6 class="mb-20">Contact Us</h6>
+								<p>
+									56/8, bir uttam qazi nuruzzaman road, west panthapath, kalabagan, Dhanmondi, Dhaka - 1205
+								</p>
+								<h3>012-6532-568-9746</h3>
+								<h3>012-6532-568-97468</h3>
+							</div>
+						</div>
+						<div class="col-lg-6  col-md-12">
+							<div class="single-footer-widget newsletter">
+								<h6>Newsletter</h6>
+								<p>You can trust us. we only send promo offers, not a single spam.</p>
+								<div id="mc_embed_signup">
+									<form target="_blank" novalidate="true" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01" method="get" class="form-inline">
 
-                  <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-3">
-                  <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
-                  <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
-                  <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
-                </ul>
-              </div>
-            </div>
-            <div class="col-md">
-              <div class="ftco-footer-widget mb-5 ml-md-4">
-                <h2 class="ftco-heading-2">Links</h2>
-                <ul class="list-unstyled">
-                  <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Home</a></li>
-                  <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>About</a></li>
-                  <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Services</a></li>
-                  <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Deparments</a></li>
-                  <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Contact</a></li>
-                </ul>
-              </div>
-              <div class="ftco-footer-widget mb-5 ml-md-4">
-                <h2 class="ftco-heading-2">Services</h2>
-                <ul class="list-unstyled">
-                  <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Neurolgy</a></li>
-                  <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Dentist</a></li>
-                  <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Ophthalmology</a></li>
-                  <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Cardiology</a></li>
-                  <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Surgery</a></li>
-                </ul>
-              </div>
-            </div>
-            <div class="col-md">
-              <div class="ftco-footer-widget mb-5">
-                <h2 class="ftco-heading-2">Recent Blog</h2>
-                <div class="block-21 mb-4 d-flex">
-                  <a class="blog-img mr-4" style="background-image: url(images/image_1.jpg);"></a>
-                  <div class="text">
-                    <h3 class="heading"><a href="#">Even the all-powerful Pointing has no control about</a></h3>
-                    <div class="meta">
-                      <div><a href="#"><span class="icon-calendar"></span> Sept. 20, 2019</a></div>
-                      <div><a href="#"><span class="icon-person"></span> Admin</a></div>
-                      <div><a href="#"><span class="icon-chat"></span> 19</a></div>
-                    </div>
-                  </div>
-                </div>
-                <div class="block-21 mb-5 d-flex">
-                  <a class="blog-img mr-4" style="background-image: url(images/image_2.jpg);"></a>
-                  <div class="text">
-                    <h3 class="heading"><a href="#">Even the all-powerful Pointing has no control about</a></h3>
-                    <div class="meta">
-                      <div><a href="#"><span class="icon-calendar"></span> Sept. 20, 2019</a></div>
-                      <div><a href="#"><span class="icon-person"></span> Admin</a></div>
-                      <div><a href="#"><span class="icon-chat"></span> 19</a></div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-md">
-                <div class="ftco-footer-widget mb-5">
-                  <h2 class="ftco-heading-2">Opening Hours</h2>
-                  <h3 class="open-hours pl-4"><span class="ion-ios-time mr-3"></span>We are open 24/7</h3>
-              </div>
-              <div class="ftco-footer-widget mb-5">
-                  <h2 class="ftco-heading-2">Subscribe Us!</h2>
-                <form action="#" class="subscribe-form">
-                  <div class="form-group">
-                    <input type="text" class="form-control mb-2 text-center" placeholder="Enter email address">
-                    <input type="submit" value="Subscribe" class="form-control submit px-3">
-                  </div>
-                </form>
-              </div>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-md-12 text-center">
+										<div class="form-group row" style="width: 100%">
+											<div class="col-lg-8 col-md-12">
+												<input name="EMAIL" placeholder="Your Email Address" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Your Email Address '" required="" type="email">
+												<div style="position: absolute; left: -5000px;">
+													<input name="b_36c4fd991d266f23781ded980_aefe40901a" tabindex="-1" value="" type="text">
+												</div>
+											</div>
 
-              <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-    Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
-            </div>
-          </div>
-        </div>
-      </footer>
+											<div class="col-lg-4 col-md-12">
+												<button class="nw-btn primary-btn">Subscribe<span class="lnr lnr-arrow-right"></span></button>
+											</div>
+										</div>
+										<div class="info"></div>
+									</form>
+								</div>
+							</div>
+						</div>
+					</div>
 
+					<div class="row footer-bottom d-flex justify-content-between">
+						<p class="col-lg-8 col-sm-12 footer-text m-0"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+						<div class="col-lg-4 col-sm-12 footer-social">
+							<a href="#"><i class="fa fa-facebook"></i></a>
+							<a href="#"><i class="fa fa-twitter"></i></a>
+							<a href="#"><i class="fa fa-dribbble"></i></a>
+							<a href="#"><i class="fa fa-behance"></i></a>
+						</div>
+					</div>
+				</div>
+			</footer>
+			<!-- End footer Area -->
 
-  <!-- loader -->
-  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
-
-
-  <script src="{{ asset ('js/jquery.min.js') }}"></script>
-  <script src="{{ asset (('js/jquery-migrate-3.0.1.min.js')) }}"></script>
-  <script src="{{ asset ('js/popper.min.js') }}"></script>
-  <script src="{{ asset ('js/bootstrap.min.js') }}"></script>
-  <script src="{{ asset ('js/jquery.easing.1.3.js') }}"></script>
-  <script src="{{ asset ('js/jquery.waypoints.min.js') }}"></script>
-  <script src="{{ asset ('js/jquery.stellar.min.js') }}"></script>
-  <script src="{{ asset ('js/owl.carousel.min.js') }}"></script>
-  <script src="{{ asset ('js/jquery.magnific-popup.min.js') }}"></script>
-  <script src="{{ asset ('js/aos.js') }}"></script>
-  <script src="{{ asset ('js/jquery.animateNumber.min.js') }}"></script>
-  <script src="{{ asset ('js/bootstrap-datepicker.js') }}"></script>
-  <script src="{{ asset ('js/jquery.timepicker.min.js') }}"></script>
-  <script src="{{ asset ('js/scrollax.min.js') }}"></script>
-  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
-  <script src="{{ asset ('js/google-map.js') }}"></script>
-  <script src="{{ asset ('js/main.js') }}"></script>
+			<script src="js/vendor/jquery-2.2.4.min.js"></script>
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+			<script src="js/vendor/bootstrap.min.js"></script>
+			<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script>
+  			<script src="js/easing.min.js"></script>
+			<script src="js/hoverIntent.js"></script>
+			<script src="js/superfish.min.js"></script>
+			<script src="js/jquery.ajaxchimp.min.js"></script>
+			<script src="js/jquery.magnific-popup.min.js"></script>
+ 			<script src="js/jquery-ui.js"></script>
+			<script src="js/owl.carousel.min.js"></script>
+			<script src="js/jquery.nice-select.min.js"></script>
+			<script src="js/mail-script.js"></script>
+			<script src="js/main.js"></script>	
 
 
 </body>
