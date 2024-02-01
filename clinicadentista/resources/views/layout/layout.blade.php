8c48753291b99ead8376500882b3b63c0c2042df
Gustavo <gsampaiosoier0@gmail.com>
2024-01-31 23:32:48 -0300

    PHP
    Feita a tradução do layout

--- clinicadentista/resources/views/layout/layout.blade.php
+++ clinicadentista/resources/views/layout/layout.blade.php
@@ -92,69 +92,66 @@
     </main>
 
     <!-- start footer Area -->
-			<footer class="footer-area section-gap">
-				<div class="container">
-					<div class="row">
-						<div class="col-lg-2  col-md-6">
-							<div class="single-footer-widget">
-								<h6>Top Products</h6>
-								<ul class="footer-nav">
-									<li><a href="#">Managed Website</a></li>
-									<li><a href="#">Manage Reputation</a></li>
-									<li><a href="#">Power Tools</a></li>
-									<li><a href="#">Marketing Service</a></li>
-								</ul>
-							</div>
-						</div>
-						<div class="col-lg-4  col-md-6">
-							<div class="single-footer-widget mail-chimp">
-								<h6 class="mb-20">Contact Us</h6>
-								<p>
-									56/8, bir uttam qazi nuruzzaman road, west panthapath, kalabagan, Dhanmondi, Dhaka - 1205
-								</p>
-								<h3>012-6532-568-9746</h3>
-								<h3>012-6532-568-97468</h3>
-							</div>
-						</div>
-						<div class="col-lg-6  col-md-12">
-							<div class="single-footer-widget newsletter">
-								<h6>Newsletter</h6>
-								<p>You can trust us. we only send promo offers, not a single spam.</p>
-								<div id="mc_embed_signup">
-									<form target="_blank" novalidate="true" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01" method="get" class="form-inline">
-
-										<div class="form-group row" style="width: 100%">
-											<div class="col-lg-8 col-md-12">
-												<input name="EMAIL" placeholder="Your Email Address" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Your Email Address '" required="" type="email">
-												<div style="position: absolute; left: -5000px;">
-													<input name="b_36c4fd991d266f23781ded980_aefe40901a" tabindex="-1" value="" type="text">
-												</div>
-											</div>
-
-											<div class="col-lg-4 col-md-12">
-												<button class="nw-btn primary-btn">Subscribe<span class="lnr lnr-arrow-right"></span></button>
-											</div>
-										</div>
-										<div class="info"></div>
-									</form>
-								</div>
-							</div>
-						</div>
+	<footer class="footer-area section-gap">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-2 col-md-6">
+					<div class="single-footer-widget">
+						<h6>Top Produtos</h6>
+						<ul class="footer-nav">
+							<li><a href="#">Website Gerenciado</a></li>
+							<li><a href="#">Gerenciar Reputação</a></li>
+							<li><a href="#">Ferramentas de Potência</a></li>
+							<li><a href="#">Serviço de Marketing</a></li>
+						</ul>
 					</div>
-
-					<div class="row footer-bottom d-flex justify-content-between">
-						<p class="col-lg-8 col-sm-12 footer-text m-0"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
-						<div class="col-lg-4 col-sm-12 footer-social">
-							<a href="#"><i class="fa fa-facebook"></i></a>
-							<a href="#"><i class="fa fa-twitter"></i></a>
-							<a href="#"><i class="fa fa-dribbble"></i></a>
-							<a href="#"><i class="fa fa-behance"></i></a>
+				</div>
+				<div class="col-lg-4 col-md-6">
+					<div class="single-footer-widget mail-chimp">
+						<h6 class="mb-20">Contate-nos</h6>
+						<p>
+							56/8, bir uttam qazi nuruzzaman road, west panthapath, kalabagan, Dhanmondi, Dhaka - 1205
+						</p>
+						<h3>012-6532-568-9746</h3>
+						<h3>012-6532-568-97468</h3>
+					</div>
+				</div>
+				<div class="col-lg-6 col-md-12">
+					<div class="single-footer-widget newsletter">
+						<h6>Boletim Informativo</h6>
+						<p>Você pode confiar em nós. Nós apenas enviamos ofertas promocionais, nenhum spam.</p>
+						<div id="mc_embed_signup">
+							<form target="_blank" novalidate="true" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01" method="get" class="form-inline">
+								<div class="form-group row" style="width: 100%">
+									<div class="col-lg-8 col-md-12">
+										<input name="EMAIL" placeholder="Seu Endereço de Email" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Seu Endereço de Email'" required="" type="email">
+										<div style="position: absolute; left: -5000px;">
+											<input name="b_36c4fd991d266f23781ded980_aefe40901a" tabindex="-1" value="" type="text">
+										</div>
+									</div>
+									<div class="col-lg-4 col-md-12">
+										<button class="nw-btn primary-btn">Inscreva<span class="lnr lnr-arrow-right"></span></button>
+									</div>
+								</div>
+								<div class="info"></div>
+							</form>
 						</div>
 					</div>
 				</div>
-			</footer>
+			</div>
+			<div class="row footer-bottom d-flex justify-content-between">
+				<p class="col-lg-8 col-sm-12 footer-text m-0"><!-- O link de volta ao Colorlib não pode ser removido. O modelo é licenciado sob CC BY 3.0. -->
+	Copyright &copy;<script>document.write(new Date().getFullYear());</script> Todos os direitos reservados | Este modelo é feito com <i class="fa fa-heart-o" aria-hidden="true"></i> por <a href="https://colorlib.com" target="_blank">Colorlib</a>
+	<!-- O link de volta ao Colorlib não pode ser removido. O modelo é licenciado sob CC BY 3.0. --></p>
+				<div class="col-lg-4 col-sm-12 footer-social">
+					<a href="#"><i class="fa fa-facebook"></i></a>
+					<a href="#"><i class="fa fa-twitter"></i></a>
+					<a href="#"><i class="fa fa-dribbble"></i></a>
+					<a href="#"><i class="fa fa-behance"></i></a>
+				</div>
+			</div>
+		</div>
+	</footer>
 			<!-- End footer Area -->
 
 			<script src="js/vendor/jquery-2.2.4.min.js"></script>
