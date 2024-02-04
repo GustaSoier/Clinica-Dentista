f2cd0ed48adcede5961691ec18e6c0d0bcef8f46
Gustavo <gsampaiosoier0@gmail.com>
2024-02-04 13:23:55 -0300

    PHP
    Criado o controller do blog

--- clinicadentista/app/Http/Controllers/blogController.php
+++ clinicadentista/app/Http/Controllers/blogController.php
@@ -4,9 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class blogController extends Controller
+class BlogController extends Controller
 {
     public function index() {
-        return view ('site.blog');
+        return view('site.blog');
     }
 }
