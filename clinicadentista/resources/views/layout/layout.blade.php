2332109b936c3959ede8f46c06dcd4f3b77b738d
Gustavo <gsampaiosoier0@gmail.com>
2024-02-09 23:17:48 -0300

    PHP
    Arrumando o formato de link

--- clinicadentista/resources/views/layout/layout.blade.php
+++ clinicadentista/resources/views/layout/layout.blade.php
@@ -154,20 +154,20 @@
 	</footer>
 			<!-- End footer Area -->
 
-			<script src="js/vendor/jquery-2.2.4.min.js"></script>
-			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-			<script src="js/vendor/bootstrap.min.js"></script>
-			<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script>
-  			<script src="js/easing.min.js"></script>
-			<script src="js/hoverIntent.js"></script>
-			<script src="js/superfish.min.js"></script>
-			<script src="js/jquery.ajaxchimp.min.js"></script>
-			<script src="js/jquery.magnific-popup.min.js"></script>
- 			<script src="js/jquery-ui.js"></script>
-			<script src="js/owl.carousel.min.js"></script>
-			<script src="js/jquery.nice-select.min.js"></script>
-			<script src="js/mail-script.js"></script>
-			<script src="js/main.js"></script>
+			<script src="{{ asset ('js/vendor/jquery-2.2.4.min.js') }}"></script>
+			<script src="{{ asset ('https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js') }}" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+			<script src="{{ asset ('js/vendor/bootstrap.min.js') }}"></script>
+			<script type="{{ asset ('text/javascript') }}" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script>
+  			<script src="{{ asset ('js/easing.min.js') }}"></script>
+			<script src="{{ asset ('js/hoverIntent.js') }}"></script>
+			<script src="{{ asset ('js/superfish.min.js') }}"></script>
+			<script src="{{ asset ('js/jquery.ajaxchimp.min.js') }}"></script>
+			<script src="{{ asset ('js/jquery.magnific-popup.min.js') }}"></script>
+ 			<script src="{{ asset ('js/jquery-ui.js') }}"></script>
+			<script src="{{ asset ('js/owl.carousel.min.js') }}"></script>
+			<script src="{{ asset ('js/jquery.nice-select.min.js') }}"></script>
+			<script src="{{ asset ('js/mail-script.js') }}"></script>
+			<script src="{{ asset ('js/main.js') }}"></script>
 
 
 </body>
