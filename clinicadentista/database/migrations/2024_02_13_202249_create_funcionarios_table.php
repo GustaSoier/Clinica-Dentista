3cee7e17c9dcc31f83681905f8e8100bd1001bcc
Gustavo <gsampaiosoier0@gmail.com>
2024-02-13 17:30:27 -0300

    Dental Care
    Adicionando os campos à tabela funcionarios

--- clinicadentista/database/migrations/2024_02_13_202249_create_funcionarios_table.php
+++ clinicadentista/database/migrations/2024_02_13_202249_create_funcionarios_table.php
@@ -15,9 +15,22 @@ return new class extends Migration
     {
         Schema::create('funcionarios', function (Blueprint $table) {
             $table->id();
+            $table->string('nomeFuncionario', 255);
+            $table->date('dataNascFuncionario');
+            $table->string('foneFuncionario', 20);
+            $table->string('enderecoFuncionario', 255);
+            $table->string('cidadeFuncionario', 100);
+            $table->string('estadoFuncionario', 50);
+            $table->string('cepFuncionario', 10);
+            $table->timestamp('dataContratacaoFuncionario');
+            $table->string('cargoFuncionario', 100);
+            $table->decimal('salarioFuncionario', 10, 2);
+            $table->enum('tipoFuncionario', ['admin', 'dentista']);
+            $table->enum('statusFuncionario', ['ativo', 'inativo']);
             $table->timestamps();
         });
     }
+
 
     /**
      * Reverse the migrations.
