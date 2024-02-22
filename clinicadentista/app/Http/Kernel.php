77de7688b2da7b3fe3c79cdb009d4872a1232f69
Gustavo Sampaio Soier <gsampaiosoier0@gmail.com>
2024-02-22 17:18:11 -0300

    Dental Care
    Trocando o middleware "AutAcademiaMiddle" para "AutClinicaMiddleware"

--- clinicadentista/app/Http/Kernel.php
+++ clinicadentista/app/Http/Kernel.php
@@ -36,6 +36,8 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+
+            \App\Http\Middleware\LogAcessoClinica::class,
         ],
 
         'api' => [
@@ -64,6 +66,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
 
-        'autenticacao' => \App\Http\Middleware\AutAcademiaMiddle::class,
+        'autenticacao' => \App\Http\Middleware\AutClinicaMiddleware::class,
     ];
 }
