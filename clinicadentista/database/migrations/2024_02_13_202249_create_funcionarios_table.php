29c8839f883392176b7b33862660b6440c020346
Gustavo Sampaio Soier <gsampaiosoier0@gmail.com>
2024-02-27 16:48:40 -0300

    Dental Care
    Retirando o idFuncionario do migrations, pois estava dando erro

--- clinicadentista/database/migrations/2024_02_13_202249_create_funcionarios_table.php
+++ clinicadentista/database/migrations/2024_02_13_202249_create_funcionarios_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('funcionarios', function (Blueprint $table) {
-            $table->id('idFuncionario');
+            $table->id();
             $table->string('nomeFuncionario', 255);
             $table->date('dataNascFuncionario');
             $table->string('foneFuncionario', 20);
