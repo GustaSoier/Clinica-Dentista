85a4f799cd69dec081a28a0ac9a53d89bd777859
Gustavo Sampaio Soier <gsampaiosoier0@gmail.com>
2024-02-27 16:43:37 -0300

    Dental Care
    Fazendo o middleware de autenticação de login da clinica

--- clinicadentista/app/Http/Middleware/AutClinicaMiddleware.php
+++ clinicadentista/app/Http/Middleware/AutClinicaMiddleware.php
@@ -1,11 +1,15 @@
 <?php
 
 namespace App\Http\Middleware;
-
+use App\Models\Funcionario;
+use App\Models\Usuario;
+use App\Models\Aluno;
+use App\Models\Funcionarios;
+use App\Models\Pacientes;
 use Closure;
 use Illuminate\Http\Request;
 
-class AutClinicaMiddleware
+class AutAcademiaMiddle
 {
     /**
      * Handle an incoming request.
@@ -14,8 +18,40 @@ class AutClinicaMiddleware
      * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
-    public function handle(Request $request, Closure $next)
+    public function handle(Request $request, Closure $next, $tipoUser)
     {
-        return $next($request);
+
+        $email = session('email');
+
+        if($email) {
+            $usuario = Usuario::where('emailUsuario', $email)->first();
+
+            if(!$usuario) {
+                return redirect()->route('login')->withErrors(['email' => 'Não autenticado']);
+            }
+
+            $tipoUsuario = $usuario -> tipo_usuario;
+
+            if($tipoUsuario) {
+                $tipo = null;
+
+                if($tipoUsuario instanceof Pacientes) {
+                    $tipo = 'paciente';
+                }
+                elseif($tipoUsuario instanceof Funcionarios) {
+                    $tipo = $tipoUsuario -> tipo_funcionario;
+                }
+            }
+        }
+
+        if($tipo === $tipoUser) {
+            return $next($request);
+        }
+        else{
+            return back()->withErrors(['email' => 'Acesso não permitido para esse perfil!']);
+        }
+
+
+
     }
 }
