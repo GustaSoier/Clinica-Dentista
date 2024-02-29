aae75849eb3daffd717de767bad03727dd889772
Gustavo Sampaio Soier <gsampaiosoier0@gmail.com>
2024-02-29 17:28:12 -0300

    Dental Care

--- clinicadentista/resources/views/dashboard/administrativo/index.blade.php
+++ clinicadentista/resources/views/dashboard/administrativo/index.blade.php
@@ -4,5 +4,37 @@
 
 @section('conteudo')
 
+<h1></h1>
+
+<table class="table table-bordered table-hover"">
+    <thead>
+        <tr>
+          <th scope="col">ID</th>
+          <th scope="col">Nome</th>
+          <th scope="col">Last</th>
+          <th scope="col">Handle</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Gustavo</td>
+          <td>Soier</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Gustavo</td>
+          <td>Soier</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td>Gustavo</td>
+          <td>Soier</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+</table>
 
 @endsection
