80b64d50d86256f62c741317be8ce0e3894696a2
Gustavo Sampaio Soier <gsampaiosoier0@gmail.com>
2024-03-01 17:18:48 -0300

    Dental Care
    Modificando a estrutura do dashboard

--- clinicadentista/resources/views/dashboard/administrativo/index.blade.php
+++ clinicadentista/resources/views/dashboard/administrativo/index.blade.php
@@ -1,40 +1,55 @@
 @extends('dashboard.layout-dash.layout')
 
-@section('title','Contato')
+@section('title', 'Administrativo')
 
 @section('conteudo')
 
-<h1></h1>
+    <div class="dash-func">
+        <h1>Funcionários</h1>
 
-<table class="table table-bordered table-hover"">
-    <thead>
-        <tr>
-          <th scope="col">ID</th>
-          <th scope="col">Nome</th>
-          <th scope="col">Last</th>
-          <th scope="col">Handle</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <th scope="row">1</th>
-          <td>Gustavo</td>
-          <td>Soier</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <th scope="row">2</th>
-          <td>Gustavo</td>
-          <td>Soier</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <th scope="row">3</th>
-          <td>Gustavo</td>
-          <td>Soier</td>
-          <td>@twitter</td>
-        </tr>
-      </tbody>
-</table>
+        <div>
+            <a href="#">Adicionar Funcionário</a>
+        </div>
+
+        <table class="table table-bordered table-hover"">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Telefone</th>
+                    <th scope="col">Cargo</th>
+                    <th scope="col">Salário</th>
+                    <th scope="col">Status</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Gustavo</td>
+                    <td>11 99999-9999</td>
+                    <td>Dentista</td>
+                    <td>R$ 2000</td>
+                    <td>Ativo</td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td>Gustavo</td>
+                    <td>11 99999-9999</td>
+                    <td>Admin</td>
+                    <td>R$ 9000</td>
+                    <td>Ativo</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td>Gustavo</td>
+                    <td>11 99999-9999</td>
+                    <td>Gerente</td>
+                    <td>R$ 5000</td>
+                    <td>Ativo</td>
+                </tr>
+            </tbody>
+        </table>
+
+    </div>
 
 @endsection
