d22f0a250099ea2faceb8f2ed800b74d56998c14
Gustavo Sampaio Soier <gsampaiosoier0@gmail.com>
2024-03-01 17:15:15 -0300

    Dental Care
    Fazendo o processo de back-end do dashboard do paciente

--- clinicadentista/app/Http/Controllers/PacienteController.php
+++ clinicadentista/app/Http/Controllers/PacienteController.php
@@ -2,11 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pacientes;
 use Illuminate\Http\Request;
 
 class PacienteController extends Controller
 {
     public function index(){
         return view('dashboard.pacientes.index');
+
+        // Recuperando o ID do aluno da sessão
+        $idPaciente = session('id');
+
+        // Buscando o paciente pelo ID no banco de dados
+        $paciente = Pacientes::find($idPaciente);
+
+        // Verificar se o paciente foi encontrado
+        if (!$paciente) {
+            // Se o aluno não for encontrado, você pode redirecionar para uma página de erro ou fazer outra ação adequada
+            abort(404, 'Paciente não encontrado');
+        }
+
+        // Passando o objeto $paciente para a view
+        // dd($paciente)
+        return view('dashboard.pacientes.index', compact('paciente'));
     }
 }
