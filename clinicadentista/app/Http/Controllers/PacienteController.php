026ddd0c6b4500bebe79880bb34767a9a544767b
Gustavo Sampaio Soier <gsampaiosoier0@gmail.com>
2024-03-04 17:12:43 -0300

    Dental Care
    update

--- clinicadentista/app/Http/Controllers/PacienteController.php
+++ clinicadentista/app/Http/Controllers/PacienteController.php
@@ -8,7 +8,6 @@ use Illuminate\Http\Request;
 class PacienteController extends Controller
 {
     public function index(){
-        return view('dashboard.pacientes.index');
 
         // Recuperando o ID do aluno da sessão
         $idPaciente = session('id');
