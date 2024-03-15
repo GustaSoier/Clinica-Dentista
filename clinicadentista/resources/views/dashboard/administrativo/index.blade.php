f9604c9fe4c3cddbe99f925c95ecc3a5ff217eab
Gustavo Sampaio Soier <gsampaiosoier0@gmail.com>
2024-03-15 13:25:00 -0300

    Dental Care
    Criando cards no index do funcionario administrativo

--- clinicadentista/resources/views/dashboard/administrativo/index.blade.php
+++ clinicadentista/resources/views/dashboard/administrativo/index.blade.php
@@ -6,4 +6,37 @@
 
 <h1>Administrativo</h1>
 
+
+<div class="row row-cols-1 row-cols-md-3 g-4">
+    <div class="col">
+      <div class="card h-200">
+
+        <div class="card-body text-center">
+            <span>255</span>
+          <h5 class="card-title">Visitas ao site</h5>
+          <p class="card-text">Quantos visitantes tivemos em nosso site.</p>
+        </div>
+
+      </div>
+    </div>
+    <div class="col">
+      <div class="card h-100">
+        <div class="card-body text-center">
+            <span>7</span>
+          <h5 class="card-title">Funcionários</h5>
+          <p class="card-text">Quantos funcionários temos cadastrados.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card h-100">
+        <div class="card-body text-center">
+            <span>28</span>
+          <h5 class="card-title">Pacientes</h5>
+          <p class="card-text">Quantos funcionários temos cadastrados.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
 @endsection
